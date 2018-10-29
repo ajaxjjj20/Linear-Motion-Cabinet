7bebbcaf1cec4a40c0b42afd1a77e924108d8c3a
ajaxjjj20 <anthonyjacques20@gmail.com>
2018-10-29 00:56:54 +0000

    Create bash file to increase the volume on stereo

---
+++ Bash/VolumeUp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+irsend SEND_ONCE harmankardon KEY_VOLUMEUP
+irsend SEND_ONCE harmankardon KEY_VOLUMEUP
+irsend SEND_ONCE harmankardon KEY_VOLUMEUP
+irsend SEND_ONCE harmankardon KEY_VOLUMEUP
+irsend SEND_ONCE harmankardon KEY_VOLUMEUP
+irsend SEND_ONCE harmankardon KEY_VOLUMEUP
+irsend SEND_ONCE harmankardon KEY_VOLUMEUP
+irsend SEND_ONCE harmankardon KEY_VOLUMEUP
+irsend SEND_ONCE harmankardon KEY_VOLUMEUP
+irsend SEND_ONCE harmankardon KEY_VOLUMEUP
